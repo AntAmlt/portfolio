1e6c84e6adb40b7f751fcd737bbdced8a4ab441a
Antoine Ameloot <antoine.ameloot.etu@univ-lille.fr>
2022-12-15 13:12:25 +0100

    Closes #2: Dynamic project item creation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+/**
+ * Project information
+ */
 var projects = [
     {
         "title": "Bobby Z Burgers",
@@ -42,6 +45,53 @@ var projects = [
     }
 ];
 
+/**
+ * Add project items
+ */
+
+ var projectItem;
+ var projectPicture;
+ var projectInfo;
+ var projectInfoContent;
+ var projectTitle;
+ var projectDate;
+ var projectShowMore;
+
+ var projectsList = document.getElementById("projects").getElementsByClassName("section-content");
+
+ projects.forEach((element, idx) => {
+    // Create HTML elements
+    projectItem = document.createElement("div");
+    projectPicture = document.createElement("img");
+    projectInfo = document.createElement("div");
+    projectInfoContent = document.createElement("div");
+    projectTitle = document.createElement("h3");
+    projectCaption = document.createElement("p");
+    projectShowMore = document.createElement("button");
+    // Add properties
+    projectItem.className = "project-item";
+    projectItem.addEventListener('click', () => openProject(idx));
+    projectPicture.src = element.picture;
+    projectPicture.alt = element.title;
+    projectInfo.className = "project-info";
+    projectInfoContent.className = "project-info-content";
+    projectTitle.innerHTML = `${element.title} <small>(${element.date})</small>`; 
+    projectCaption.innerText = element.shortDescription;
+    projectShowMore.addEventListener('click', () => openProject(idx));
+    projectShowMore.innerText = "Voir plus";
+    // Nest elements into each other
+    projectInfoContent.appendChild(projectTitle);
+    projectInfoContent.appendChild(projectCaption);
+    projectInfo.appendChild(projectInfoContent);
+    projectInfo.appendChild(projectShowMore);
+    projectItem.appendChild(projectPicture);
+    projectItem.appendChild(projectInfo);
+    // Add the project item in the page
+    projectsList[0].appendChild(projectItem);
+ });
+
+
+
 var projectModal = document.getElementById("project-modal");
 var headerProjectModal = document.getElementById("modal-header");
 var titleProjectModal = document.getElementById("project-modal-title");
@@ -52,7 +102,10 @@ var linksProjectModal = document.getElementById("project-modal-links");
 
 
 
-
+/**
+ * Open a modal with project informations since its index.
+ * @param {*} projectIndex The project index
+ */
 function openProject(projectIndex) {
     if (0 <= projectIndex && projectIndex < projects.length) {
         removeAllChildNodes(technologiesProjectModal);
@@ -73,13 +126,20 @@ function openProject(projectIndex) {
             createLinkElement(element);
         })
         projectModal.style.display = "block";
-    }
+    } else console.log("Test");
 }
 
+/**
+ * Close the project modal.
+ */
 function closeProject() {
     projectModal.style.display = "none";
 }
 
+/**
+ * Create and insert the technology tag used in a project.
+ * @param {*} technology The technology used in the project.
+ */
 function createTechnologyElement(technology) {
     const element = document.createElement("span");
     element.className = "project-modal-technology";
@@ -87,6 +147,10 @@ function createTechnologyElement(technology) {
     technologiesProjectModal.appendChild(element);
 }
 
+/**
+ * Create and insert a link for a project.
+ * @param {*} link The link (title and URL) to add.
+ */
 function createLinkElement(link) {
     const element = document.createElement("a");
     element.href = link.link;
@@ -94,6 +158,10 @@ function createLinkElement(link) {
     linksProjectModal.appendChild(element);
 }
 
+/**
+ * Remove all children from a parent.
+ * @param {*} parent The parent of which the children have to be deleted.
+ */
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
